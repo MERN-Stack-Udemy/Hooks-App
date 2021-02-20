f4d29c752a49e1a2455d856734c9faa677dcc0cc
abdiel_249 <abdielorellana3@gmail.com>
2021-02-20 19:17:20 -0400

    test on <TodoList />

--- src/tests/components/08-useReducer/TodoList.test.js
+++ src/tests/components/08-useReducer/TodoList.test.js
@@ -4,14 +4,27 @@ import { demoTodos } from "../fixtems/demoTodos"
 
 describe('Tests on <TodoList />', () => {
   
-  const wrapper = shallow( <TodoList todos={ demoTodos }/>)
+  const dispatch = jest.fn();
+  const wrapper = shallow( 
+    <TodoList 
+      todos={ demoTodos }
+      dispatch = { dispatch }
+    />
+  )
 
   test('should match to snapshot', () => {
     expect( wrapper ).toMatchSnapshot()
   })
   
-  test('should display all todos', () => {
-    
+  test('should display all ToDos', () => {
+    expect( wrapper.find('TodoListItem').length ).toBe( demoTodos.length );
   })
+
+  test('should cointains all properties on <TodoListItem/>', () => {
+    const index = 0;
+    expect( wrapper.find('TodoListItem').at(index).prop('dispatch') ).toEqual( expect.any(Function) );
+    expect( wrapper.find('TodoListItem').at(index).prop('todo') ).toEqual( demoTodos[index] );
+  })
+  
   
 })
